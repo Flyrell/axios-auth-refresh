dd2875796b4c415e9eca74b5b2c65dac1bc79bd8
Dawid Zbiński <dawid@zbinski.dev>
2019-09-13 12:20:15 +0200

    beta

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,22 +1,26 @@
-import { AxiosInstance } from "axios";
+import { AxiosInstance, AxiosRequestConfig, AxiosStatic } from "axios";
 export interface AxiosAuthRefreshOptions {
+    instance?: AxiosInstance;
     statusCodes?: Array<number>;
 }
 export interface AxiosAuthRefreshCache {
     refreshCall: Promise<any> | undefined;
     requestQueueInterceptorId: number | undefined;
 }
+export interface AxiosAuthRefreshRequestConfig extends AxiosRequestConfig {
+    skipAuthRefresh?: boolean;
+}
 /**
  * Creates an authentication refresh interceptor that binds to any error response.
  * If the response code is 401, interceptor tries to call the refreshTokenCall which must return a Promise.
  * While refreshTokenCall is running, all new requests are intercepted and waiting for it to resolve.
  * After Promise is resolved/rejected the authentication refresh interceptor is revoked.
- * @param {AxiosInstance} axios - axios instance
+ * @param {AxiosStatic} axios - axios static object
  * @param {(error: any) => Promise<AxiosPromise>} refreshTokenCall - refresh token call which must return a Promise
  * @param {AxiosAuthRefreshOptions} options - options for the interceptor @see defaultOptions
  * @return {number} - interceptor id (in case you want to eject it manually)
  */
-export default function createAuthRefreshInterceptor(axios: AxiosInstance, refreshTokenCall: (error: any) => Promise<any>, options?: AxiosAuthRefreshOptions): number;
+export default function createAuthRefreshInterceptor(axios: AxiosStatic, refreshTokenCall: (error: any) => Promise<any>, options?: AxiosAuthRefreshOptions): number;
 /**
  * Merges two config objects (master rewrites slave)
  */
@@ -33,4 +37,4 @@ export declare function createRefreshCall(error: any, fn: (error: any) => Promis
 /**
  * Creates refresh call if it does not exist or returns the existing one
  */
-export declare function createRequestQueueInterceptor(axios: AxiosInstance, cache: AxiosAuthRefreshCache): number;
+export declare function createRequestQueueInterceptor(axios: AxiosStatic, cache: AxiosAuthRefreshCache): number;
