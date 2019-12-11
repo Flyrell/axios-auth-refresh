ed98d87059a4be1c5786da621d0f6046eb0542f4
Anton Komarev <1849174+antonkomarev@users.noreply.github.com>
2019-12-11 22:20:45 +0300

    Extract resend failed request method (#51)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
+import axios, {AxiosInstance, AxiosPromise, AxiosRequestConfig, AxiosResponse} from 'axios';
 
 // Types
 
@@ -75,10 +75,7 @@ export default function createAuthRefreshInterceptor(
         return refreshing
             .finally(() => unsetCache(instance, cache))
             .catch(error => Promise.reject(error))
-            .then(() => {
-                error.config.skipAuthRefresh = true;
-                return axios(error.response.config);
-            });
+            .then(() => resendFailedRequest(error));
     });
 }
 
@@ -176,4 +173,17 @@ export function unsetCache(
     cache.requestQueueInterceptorId = undefined;
     cache.refreshCall = undefined;
     cache.skipInstances = cache.skipInstances.filter(skipInstance => skipInstance !== instance);
+}
+
+/**
+ * Resend failed axios request.
+ *
+ * @param {any} error
+ * @return AxiosPromise
+ */
+function resendFailedRequest(
+    error: any,
+): AxiosPromise {
+    error.config.skipAuthRefresh = true;
+    return axios(error.response.config);
 }
