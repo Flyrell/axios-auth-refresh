32b650c84aa782bba53a2f2e9728a650fa723744
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-07-25 08:45:31 +0200

    Run Prettier again

--- src/model.ts
+++ src/model.ts
@@ -1,32 +1,32 @@
 import { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
 
 export interface AxiosAuthRefreshOptions {
-  statusCodes?: Array<number>;
-  /**
-   * Determine whether to refresh, if "shouldRefresh" is configured, The "statusCodes" logic will be ignored
-   * @param error AxiosError
-   * @returns boolean
-   */
-  shouldRefresh?(error: AxiosError): boolean;
-  retryInstance?: AxiosInstance;
-  interceptNetworkError?: boolean;
-  pauseInstanceWhileRefreshing?: boolean;
-  onRetry?: (requestConfig: AxiosRequestConfig) => AxiosRequestConfig | Promise<AxiosRequestConfig>;
+    statusCodes?: Array<number>;
+    /**
+     * Determine whether to refresh, if "shouldRefresh" is configured, The "statusCodes" logic will be ignored
+     * @param error AxiosError
+     * @returns boolean
+     */
+    shouldRefresh?(error: AxiosError): boolean;
+    retryInstance?: AxiosInstance;
+    interceptNetworkError?: boolean;
+    pauseInstanceWhileRefreshing?: boolean;
+    onRetry?: (requestConfig: AxiosRequestConfig) => AxiosRequestConfig | Promise<AxiosRequestConfig>;
 
-  /**
-   * @deprecated
-   * This flag has been deprecated in favor of `pauseInstanceWhileRefreshing` flag.
-   * Use `pauseInstanceWhileRefreshing` instead.
-   */
-  skipWhileRefreshing?: boolean;
+    /**
+     * @deprecated
+     * This flag has been deprecated in favor of `pauseInstanceWhileRefreshing` flag.
+     * Use `pauseInstanceWhileRefreshing` instead.
+     */
+    skipWhileRefreshing?: boolean;
 }
 
 export interface AxiosAuthRefreshCache {
-  skipInstances: AxiosInstance[];
-  refreshCall: Promise<any>|undefined;
-  requestQueueInterceptorId: number|undefined;
+    skipInstances: AxiosInstance[];
+    refreshCall: Promise<any> | undefined;
+    requestQueueInterceptorId: number | undefined;
 }
 
 export interface AxiosAuthRefreshRequestConfig extends AxiosRequestConfig {
-  skipAuthRefresh?: boolean;
+    skipAuthRefresh?: boolean;
 }
